502cdc9831d03245e74639130df92a7838277105
achaiah <Alexei.Samoylov@digikey.com>
2019-03-08 10:09:58 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='wick',
+      version='0.5.0',
+      description='High-level batteries-included training library for Pytorch',
+      author='Achaiah',
+      author_email='n/a',
+      packages=find_packages()
+     )
